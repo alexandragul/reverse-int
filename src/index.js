1eeee287ee47fce3baad53cc352866caa047f4f9
alexandragul <72790705+alexandragul@users.noreply.github.com>
2021-02-11 21:25:09 +0300

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
+  let arrayOfDigits = n.toString().split('');
   
+  if (arrayOfDigits[0] == '-') {
+    arrayOfDigits.splice(0, 1);
+  }
+  
+  let reversedNumber = +arrayOfDigits.reverse().join('');
+  return reversedNumber;
 }
